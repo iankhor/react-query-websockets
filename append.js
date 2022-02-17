03e0a34b93675ad2f7ab327dcac387d058253d7e
iankhor <iankhorbc.thecoder@gmail.com>
2022-02-18 07:38:35 +1100

    refactor and add append progress

--- append.js
+++ append.js
@@ -6,14 +6,29 @@ switch (process.argv[2]) {
   case 'clean':
     file.posts = []
     break;
+  case 'progress-init':
+    file.posts.push(
+      {
+        "id": 'progress-123',
+        "progress": "*"
+      }
+    )
+    break;
+  case 'progress':
+    const f = file.posts.find(f => f.id === 'progress-123')
+    f.progress+="*"
+    break;
   default:
-    file.posts.push([
-      { "id": new Date().getTime(), "title": `title-${new Date().getTime()}`, "author": `author-${new Date().getTime()}` }
-    ])
+    file.posts.push(
+      {
+        "id": new Date().getTime(),
+        "progress": "*"
+      }
+    )
 }
 
 fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
   if (err) return console.log(err);
-  console.log(JSON.stringify(file));
-  console.log('writing to ' + fileName);
+  // console.log(JSON.stringify(file));
+  // console.log('writing to ' + fileName);
 });
