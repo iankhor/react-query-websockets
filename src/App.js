4c4cd99e70a820eb1adada91e3e733f1928ea19c
iankhor <iankhorbc.thecoder@gmail.com>
2022-02-17 23:23:57 +1100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import './App.css';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import { useQuery } from "react-query";
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Server } from 'mock-socket';
 import { QueryClient, QueryClientProvider } from "react-query";
 
@@ -25,8 +23,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
